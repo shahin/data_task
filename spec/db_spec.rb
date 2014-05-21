cabf3d1b7d2ef0bad169f82e60fb622fd59693c4
Shahin Saneinejad <shahin.saneinejad@gmail.com>
2014-05-20 20:13:34 -0700

    Add tests for table creation, table drops

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -25,7 +25,19 @@ module Rake
         expect(Db.table_exists?(test_table)).to be_false
       end
 
+      it "creates a table when called to" do
+        Db.create_table test_table, nil, '(var1 text)'
+        expect(Db.table_exists?(test_table)).to be_true
+      end
+
+      it "drops a table when called to" do
+        Db.create_table test_table, nil, '(var1 text)'
+        Db.drop_table test_table
+        expect(Db.table_exists?(test_table)).to be_false
+      end
+
       it "says it is tracking tables after tracking is set up" do
+        Db.tear_down_tracking
         Db.set_up_tracking
         expect(Db.tracking_tables?).to be_true
       end
