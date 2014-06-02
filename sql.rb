7cce5a50bf4e2c416ad13130012c509aca9f6ca7
Shahin Saneinejad <shahin.saneinejad@gmail.com>
2014-06-01 18:59:21 -0700

    Add method docstrings to the Sql module

--- sql.rb
+++ sql.rb
@@ -17,6 +17,12 @@ module Rake
         Db.falsey_value
       end
 
+      # Parse a single string value into an object using the supplied type logic.
+      #
+      # @param r [Array] an array (table) of arrays (rows), usually resulting from a database query
+      # @param &type_logic [Block] code that takes the first 
+      # @raise [TypeError] if r contains more than one row or column
+      # @returns [Object] the return value of &type_logic on the value in r
       def self.parse_single_value r, &type_logic
         if r.nil? || r.empty? || r == [[]] || r == [[nil]]
           return nil
@@ -28,6 +34,10 @@ module Rake
         yield(r)
       end
 
+      # Get a single integer via SQL.
+      #
+      # @param sql [String] the SQL to produce the integer
+      # @returns [Integer] any valid value for this Ruby type
       def self.get_single_int sql
         r = get_array(sql)
         parse_single_value r do
@@ -35,6 +45,10 @@ module Rake
         end
       end
 
+      # Get a single time via SQL.
+      #
+      # @param sql [String] the SQL to produce the time string with timezone info
+      # @returns [Time] the time produced by the SQL, converted to Ruby's local time
       def self.get_single_time sql
         r = get_array(sql)
         parse_single_value r do
