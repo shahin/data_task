f44e92622a9f5feb1ed71831c6c69786611b6b47
Shahin Saneinejad <shahin.saneinejad@gmail.com>
2014-06-29 23:21:13 -0700

    Fix tests by fixing requires, config path

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 require 'rake/testtask'
 require 'bundler/gem_tasks'
-require 'db'
+require 'table_task/db'
 require 'table_task'
 
 Rake::TestTask.new do |t|
--- test/helper.rb
+++ test/helper.rb
@@ -8,10 +8,10 @@ require 'minitest-spec-context'
 
 require 'logger'
 
-require_relative '../lib/sql.rb'
-require_relative '../lib/db.rb'
-require_relative '../lib/table.rb'
-require_relative '../lib/table_task.rb'
+require 'table_task/sql'
+require 'table_task/db'
+require 'table_task/table'
+require 'table_task'
 
 def with_tracking &ops
   Rake::TableTask::Db.set_up_tracking
