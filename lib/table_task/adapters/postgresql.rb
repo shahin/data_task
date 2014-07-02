1cbd98661ebfa0d647ff633c69749ab5b4b7c6cb
Shahin Saneinejad <shahin.saneinejad@gmail.com>
2014-07-01 23:44:37 -0700

    Begin conversion to an instance-based framework
    1. Turn Db into a standard base class, instead of an abstract singleton
    2. Change the PostgreSQL adapter to work as an instantiated class
    3. Remove Db references from Sql, since it has no access to
       instantiated adapters.
    
    Ran tracking setup/teardown in Pry. No tests were run.

--- lib/table_task/adapters/postgresql.rb
+++ lib/table_task/adapters/postgresql.rb
@@ -1,15 +1,32 @@
 require 'pg'
-require_relative 'support/transactions'
-require_relative 'support/booleans'
+require_relative 'support/transactions.rb'
+require_relative 'support/booleans.rb'
 
 module Rake
   module TableTask
 
     class PostgreSQL < Db
 
-      @@adapters[:postgresql] = self
+      def initialize host, port, database, username, password=nil
+        @connection = PG::Connection.new(
+          host || 'localhost',
+          port || 5432,
+          nil,
+          nil,
+          database,
+          username,
+          password || ''
+        )
+        @connection.set_notice_processor do |msg|
+          if msg =~ /^ERROR:/
+            LOG.error('psql') { msg.gsub(/\n/,'; ') }
+          else
+            LOG.info('psql') { msg.gsub(/\n/,'; ') }
+          end
+        end
+      end
 
-      def self.table_tracker_columns
+      def table_tracker_columns
         # upcase all enum'd column values because system tables store them in upcase
         cols = super
         cols.each do |k1,v1|
@@ -27,26 +44,7 @@ module Rake
         cols
       end
 
-      def self.connect
-        @connection = PG::Connection.new(
-          config['host'] || 'localhost',
-          config['port'] || 5432,
-          nil,
-          nil,
-          config['database'],
-          config['username'],
-          config['password'] || ''
-        )
-        @connection.set_notice_processor do |msg|
-          if msg =~ /^ERROR:/
-            LOG.error('psql') { msg.gsub(/\n/,'; ') }
-          else
-            LOG.info('psql') { msg.gsub(/\n/,'; ') }
-          end
-        end
-      end
-
-      def self.execute sql
+      def execute sql
         connect if @connection.nil?
         begin
           r = @connection.exec sql
@@ -57,14 +55,14 @@ module Rake
         end
       end
 
-      extend StandardBooleans
-      extend StandardTransactions
+      include StandardBooleans
+      include StandardTransactions
 
-      def self.tracking_tables?
+      def tracking_tables?
         table_exists?(TABLE_TRACKER_NAME)
       end
 
-      def self.set_up_tracking
+      def set_up_tracking
         tear_down_tracking
         column_definitions = table_tracker_columns.map do |col,col_defn|
           col.to_s + ' ' + col_defn[:data_type].to_s
@@ -72,36 +70,38 @@ module Rake
         create_table TABLE_TRACKER_NAME, nil, " (#{column_definitions})", false
       end
 
-      def self.tear_down_tracking
+      def tear_down_tracking
         drop_table TABLE_TRACKER_NAME
       end
       
-      def self.reset_tracking
+      def reset_tracking
         truncate_table TABLE_TRACKER_NAME
       end
 
-      def self.table_mtime table_name
-        Sql.get_single_time <<-EOSQL
+      def table_mtime table_name
+        Sql.get_single_time(
+        execute <<-EOSQL
           select max(time) 
           from #{TABLE_TRACKER_NAME} 
           where relation_name = '#{table_name}'
         EOSQL
+        )
       end
 
-      def self.truncate_table table_name
+      def truncate_table table_name
         return if table_name.casecmp(TABLE_TRACKER_NAME) == 0
-        Db.execute "truncate table #{table_name}"
+        execute "truncate table #{table_name}"
         track_truncate table_name
       end
 
-      def self.drop_table table_name
-        Db.execute "drop table if exists #{table_name} cascade"
+      def drop_table table_name
+        execute "drop table if exists #{table_name} cascade"
         return if table_name.casecmp(TABLE_TRACKER_NAME) == 0
         track_drop table_name
       end
 
-      def self.track_drop table_name
-        Db.execute <<-EOSQL
+      def track_drop table_name
+        execute <<-EOSQL
           delete from #{TABLE_TRACKER_NAME} 
           where 
             relation_name = '#{table_name}' and 
@@ -109,17 +109,17 @@ module Rake
         EOSQL
       end
 
-      def self.table_exists? table_name, options = {}
+      def table_exists? table_name, options = {}
         relation_exists? table_name, :table, options
       end
 
-      def self.view_exists? view_name, options = {}
+      def view_exists? view_name, options = {}
         relation_exists? view_name, :view, options
       end
 
-      def self.create_table table_name, data_definition, column_definitions, track_table=true
+      def create_table table_name, data_definition, column_definitions, track_table=true
         drop_table table_name
-        Db.execute <<-EOSQL
+        execute <<-EOSQL
           create table #{table_name} #{column_definitions}
           #{ "as #{data_definition}" if !data_definition.nil? }
         EOSQL
@@ -129,19 +129,19 @@ module Rake
         end
       end
 
-      def self.create_view view_name, view_definition
+      def create_view view_name, view_definition
         drop_view view_name
-        Db.execute <<-EOSQL
+        execute <<-EOSQL
           create view #{view_name} as
           #{view_definition}
         EOSQL
       end
 
-      def self.drop_view view_name
-        Db.execute "drop view if exists #{view_name} cascade"
+      def drop_view view_name
+        execute "drop view if exists #{view_name} cascade"
       end
 
-      def self.operations_supported
+      def operations_supported
         {
           :by_db => operations_supported_by_db,
           :by_app => [:truncate, :create] - operations_supported_by_db
@@ -152,22 +152,22 @@ module Rake
 
       private
 
-        def self.operations_supported_by_db
+        def operations_supported_by_db
           operations_supported_by_db_rules
         end
 
-        def self.operations_supported_by_db_rules
+        def operations_supported_by_db_rules
           [:update, :insert, :delete]
         end
 
-        def self.rule_name table_name, operation
+        def rule_name table_name, operation
           "#{table_name}_#{operation.to_s}"
         end
 
-        def self.create_tracking_rules table_name
+        def create_tracking_rules table_name
           operations_supported_by_db_rules.each do |operation|
-            Db.execute <<-EOSQL
-              create or replace rule #{self.rule_name(table_name, operation)} as 
+            execute <<-EOSQL
+              create or replace rule #{rule_name(table_name, operation)} as 
                 on #{operation.to_s} to #{table_name} do also (
 
                   delete from #{TABLE_TRACKER_NAME} where 
@@ -187,9 +187,9 @@ module Rake
           end
         end
 
-        def self.track_creation table_name, n_tuples
+        def track_creation table_name, n_tuples
           operation = :create
-          Db.execute <<-EOSQL
+          execute <<-EOSQL
             delete from #{TABLE_TRACKER_NAME} where
               relation_name = '#{table_name}' and
               relation_type = '#{relation_type_values[:table]}'
@@ -203,8 +203,8 @@ module Rake
           EOSQL
         end
 
-        def self.track_truncate table_name
-          Db.execute <<-EOSQL
+        def track_truncate table_name
+          execute <<-EOSQL
             update #{TABLE_TRACKER_NAME}
             set 
               operation = '#{operation_values[:truncate]}',
@@ -215,7 +215,7 @@ module Rake
           EOSQL
         end
 
-        def self.relation_exists? relation_name, relation_type, options = {}
+        def relation_exists? relation_name, relation_type, options = {}
           options = { :schema_names => nil }.merge(options)
 
           if !options[:schema_names].nil?
@@ -224,7 +224,8 @@ module Rake
             schema_conditions_sql = 'true'
           end
 
-          n_matches = Sql.get_single_int <<-EOSQL 
+          n_matches = Sql.get_single_int(
+          execute <<-EOSQL 
             select count(*)
             from information_schema.tables 
             where 
@@ -232,6 +233,7 @@ module Rake
               table_type = '#{relation_type_values[relation_type]}' and
               #{ schema_conditions_sql }
           EOSQL
+          )
           (n_matches > 0)
         end
 
