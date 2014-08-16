7e06d8ab380b31c291aa287b20b7d0349dbdb7a3
Shahin Saneinejad <shahin.saneinejad@gmail.com>
2014-08-16 15:55:28 -0700

    Break tests with reorganization

--- test/helper.rb
+++ test/helper.rb
@@ -27,7 +27,7 @@ module TestHelper
 
   def self.get_adapter_to_test_db
     # connect an adapter to the configured database for testing
-    config = YAML.load_file('test/config/database.yml')[ENV['DATATASK_ENV'] || 'postgres_test']
+    config = YAML.load_file('test/config/database.yml')[ENV['DATATASK_ENV'] || 'sqlite_test']
     klass = "Rake::DataTask::#{config['adapter'].capitalize}".split('::').inject(Object) {|memo, name| memo = memo.const_get(name); memo}
     adapter = klass.new(config)
 
@@ -38,3 +38,38 @@ module TestHelper
   end
 end
 
+
+module Rake
+  module DataTask
+    module DataCreation
+      OLDDATA = "old_data"
+      NEWDATA = "new_data"
+
+      def create_timed_data(adapter, old_data_name, *new_data_names)
+        old_data = Data.new(old_data_name, adapter)
+        return if (old_data.exists? &&
+          new_data_names.all? do |new_data_name|
+            new_data = Data.new(new_data_name, adapter)
+            new_data.exists? && new_data.mtime > old_data.mtime
+          end)
+        now = Time.now
+
+        create_data(adapter, old_data_name)
+        sleep(1.0)
+
+        new_data_names.each do |new_data_name|
+          create_data(adapter, new_data_name)
+        end
+      end
+
+      def create_data(adapter, name)
+        adapter.create_data name, nil, '(var1 integer, var2 integer)' unless adapter.data_exists?(name)
+        adapter.data_mtime(name)
+      end
+
+      def drop_data(adapter, name)
+        adapter.drop_data(name) rescue nil
+      end
+    end
+  end
+end
