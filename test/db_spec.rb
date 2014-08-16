6914dc520f4875ace78e966fc29764645cdffdca
Shahin Saneinejad <shahin.saneinejad@gmail.com>
2014-08-16 13:29:11 -0700

    Change Db tests from spec to normal tests

--- test/db_spec.rb
+++ test/db_spec.rb
@@ -1,199 +1,208 @@
+require File.expand_path(
+  File.join(Gem::Specification.find_by_name('rake').gem_dir,'test/helper.rb'), __FILE__)
+
 require_relative './helper.rb'
+require 'data_task/adapters/postgres'
+
 
 module Rake
   module DataTask
 
-    describe Db do
+    class DbTest < Rake::TestCase
 
-      around do |test|
+      def initialize *args
+        super
+
+        @test_table = "test_table"
+        @test_view = "test_view"
+      end
+
+      def around(&block)
         @adapter = TestHelper.get_adapter_to_test_db
         @adapter.with_transaction_rollback do
-          test.call
+          yield
         end
       end
 
-      test_table = "test_table"
-      test_view = "test_view"
-
-      it "finds a table when it exists" do
-        if !@adapter.table_exists?(test_table)
+      def test_finds_a_table_when_it_exists
+        if !@adapter.table_exists?(@test_table)
           @adapter.execute <<-EOSQL
-            create table #{test_table} (var1 text)
+            create table #{@test_table} (var1 text)
           EOSQL
         end
-        @adapter.table_exists?(test_table).must_equal true
+        assert_equal @adapter.table_exists?(@test_table), true
       end
 
-      it "does not find a table when it does not exist" do
-        if @adapter.table_exists?(test_table)
+      def test_does_not_find_a_table_when_it_does_not_exist
+        if @adapter.table_exists?(@test_table)
           @adapter.execute <<-EOSQL
-            drop table #{test_table}
+            drop table #{@test_table}
           EOSQL
         end
-        @adapter.table_exists?(test_table).must_equal false
+        assert_equal @adapter.table_exists?(@test_table), false
       end
 
-      it "creates a table when called to" do
+      def test_creates_a_table_when_called_to
         @adapter.with_tracking do
 
-          @adapter.create_table test_table, nil, '(var1 text)'
-          @adapter.table_exists?(test_table).must_equal true
+          @adapter.create_table @test_table, nil, '(var1 text)'
+          assert_equal @adapter.table_exists?(@test_table), true
 
         end
       end
 
-      it "drops a table when called to" do
+      def test_drops_a_table_when_called_to
         @adapter.with_tracking do
 
-          @adapter.create_table test_table, nil, '(var1 text)'
-          @adapter.drop_table test_table
-          @adapter.table_exists?(test_table).must_equal false
+          @adapter.create_table @test_table, nil, '(var1 text)'
+          @adapter.drop_table @test_table
+          assert_equal @adapter.table_exists?(@test_table), false
 
         end
       end
 
-      it "creates a view when called to" do
+      def test_creates_a_view_when_called_to
         @adapter.with_tracking do
 
-          @adapter.create_table test_table, nil, '(var1 text)'
-          @adapter.create_view test_view, "select * from #{test_table}"
-          @adapter.view_exists?(test_view).must_equal true
+          @adapter.create_table @test_table, nil, '(var1 text)'
+          @adapter.create_view @test_view, "select * from #{@test_table}"
+          assert_equal @adapter.view_exists?(@test_view), true
 
         end
       end
 
-      it "drops a view when called to" do
+      def test_drops_a_view_when_called_to
         @adapter.with_tracking do
 
-          @adapter.create_table test_table, nil, '(var1 text)'
-          @adapter.create_view test_view, "select * from #{test_table}"
-          @adapter.drop_view test_view
-          @adapter.view_exists?(test_view).must_equal false
+          @adapter.create_table @test_table, nil, '(var1 text)'
+          @adapter.create_view @test_view, "select * from #{@test_table}"
+          @adapter.drop_view @test_view
+          assert_equal @adapter.view_exists?(@test_view), false
 
         end
       end
 
-      it "drops a view when the underlying table is dropped" do
+      def test_drops_a_view_when_the_underlying_table_is_dropped
         @adapter.with_tracking do
 
-          @adapter.create_table test_table, nil, '(var1 text)'
-          @adapter.create_view test_view, "select * from #{test_table}"
-          @adapter.drop_table test_table
-          @adapter.view_exists?(test_view).must_equal false
+          @adapter.create_table @test_table, nil, '(var1 text)'
+          @adapter.create_view @test_view, "select * from #{@test_table}"
+          @adapter.drop_table @test_table
+          assert_equal @adapter.view_exists?(@test_view), false
 
         end
       end
 
-      it "updates the tracking table when it creates a table" do
+      def test_updates_the_tracking_table_when_it_creates_a_table
         @adapter.with_tracking do
 
-          @adapter.create_table test_table, nil, '(var1 text)'
+          @adapter.create_table @test_table, nil, '(var1 text)'
            tracked_create = Sql.get_single_int( 
             @adapter.execute <<-EOSQL
             select 1 from #{Db::TABLE_TRACKER_NAME} 
             where 
-              relation_name = '#{test_table}' and
+              relation_name = '#{@test_table}' and
               relation_type = '#{@adapter.relation_type_values[:table]}' and
               operation = '#{@adapter.operation_values[:create]}'
             EOSQL
           )
-          tracked_create.must_equal 1
+          assert_equal tracked_create, 1
 
         end
       end
 
-      it "updates the tracking table when it drops a table" do
+      def test_updates_the_tracking_table_when_it_drops_a_table
         @adapter.with_tracking do
 
-          @adapter.create_table test_table, nil, '(var1 text)'
-          @adapter.drop_table test_table
+          @adapter.create_table @test_table, nil, '(var1 text)'
+          @adapter.drop_table @test_table
           still_tracking_table = Sql.get_single_int(
             @adapter.execute <<-EOSQL
             select 1 from #{Db::TABLE_TRACKER_NAME} 
             where 
-              relation_name = '#{test_table}' and
+              relation_name = '#{@test_table}' and
               relation_type = '#{@adapter.relation_type_values[:table]}'
             EOSQL
           )
-          still_tracking_table.must_be_nil
+          assert_nil still_tracking_table
 
         end
       end
 
-      it "updates the tracking table on insert to a tracked table" do
+      def test_updates_the_tracking_table_on_insert_to_a_tracked_table
         @adapter.with_tracking do
 
-          @adapter.create_table test_table, nil, '(var1 text)'
+          @adapter.create_table @test_table, nil, '(var1 text)'
           @adapter.execute <<-EOSQL
-            insert into #{test_table} values ('a')
+            insert into #{@test_table} values ('a')
           EOSQL
           tracked_insert = Sql.get_single_int( 
             @adapter.execute <<-EOSQL
             select 1 from #{Db::TABLE_TRACKER_NAME} 
             where 
-              relation_name = '#{test_table}' and
+              relation_name = '#{@test_table}' and
               relation_type = '#{@adapter.relation_type_values[:table]}' and
               operation = '#{@adapter.operation_values[:insert]}'
             EOSQL
           )
-          tracked_insert.must_equal 1
+          assert_equal tracked_insert, 1
 
         end
       end
 
-      it "updates the tracking table on update on a tracked table" do
+      def test_updates_the_tracking_table_on_update_on_a_tracked_table
         @adapter.with_tracking do
 
-          @adapter.create_table test_table, nil, '(var1 text, var2 text)'
+          @adapter.create_table @test_table, nil, '(var1 text, var2 text)'
           @adapter.execute <<-EOSQL
-            insert into #{test_table} values ('a', 'a')
+            insert into #{@test_table} values ('a', 'a')
           EOSQL
           @adapter.execute <<-EOSQL
-            update #{test_table} set var2 = 'b' where var1 = 'a'
+            update #{@test_table} set var2 = 'b' where var1 = 'a'
           EOSQL
 
           tracked_insert = Sql.get_single_int( 
             @adapter.execute <<-EOSQL
             select 1 from #{Db::TABLE_TRACKER_NAME} 
             where 
-              relation_name = '#{test_table}' and
+              relation_name = '#{@test_table}' and
               relation_type = '#{@adapter.relation_type_values[:table]}' and
               operation = '#{@adapter.operation_values[:update]}'
             EOSQL
           )
-          tracked_insert.must_equal 1
+          assert_equal tracked_insert, 1
 
         end
       end
 
-      it "updates the tracking table on truncate of a tracked table" do
+      def test_updates_the_tracking_table_on_truncate_of_a_tracked_table
         @adapter.with_tracking do
 
-          @adapter.create_table test_table, nil, '(var1 text)'
-          @adapter.truncate_table test_table
+          @adapter.create_table @test_table, nil, '(var1 text)'
+          @adapter.truncate_table @test_table
           tracked_truncate = Sql.get_single_int( 
             @adapter.execute <<-EOSQL
             select 1 from #{Db::TABLE_TRACKER_NAME} 
             where 
-              relation_name = '#{test_table}' and
+              relation_name = '#{@test_table}' and
               relation_type = '#{@adapter.relation_type_values[:table]}' and
               operation = '#{@adapter.operation_values[:truncate]}'
             EOSQL
           )
-          tracked_truncate.must_equal 1
+          assert_equal tracked_truncate, 1
 
         end
       end
 
-      it "says it is tracking tables after tracking is set up" do
+      def test_says_it_is_tracking_tables_after_tracking_is_set_up
         @adapter.tear_down_tracking
         @adapter.set_up_tracking
-        @adapter.tracking_tables?.must_equal true
+        assert_equal @adapter.tracking_tables?, true
       end
 
-      it "says it is not tracking tables after tracking is torn down" do
+      def test_says_it_is_not_tracking_tables_after_tracking_is_torn_down
         @adapter.tear_down_tracking
-        (@adapter.tracking_tables?).must_equal false
+        assert_equal (@adapter.tracking_tables?), false
       end
 
     end
